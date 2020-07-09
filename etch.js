6b8bff0ae75b45fff9f458a2faf3dc478a7e8fc7
Kevin <kevin03.nguyen@gmail.com>
2020-07-09 00:20:08 -0400

    background color changes when hovering over grid items

--- etch.js
+++ etch.js
@@ -1,4 +1,7 @@
 
+function hover(){
+    this.style.cssText = "background-color: red;";
+}
 
 function grid(){
     let container = document.querySelector(".container");
@@ -8,12 +11,26 @@ function grid(){
         let grid = document.createElement("div");
         grid.classList.add("box");
         grid.classList.add(`box${i}`);
-        grid.style.cssText = "background-color: green;";
+       
         grid.textContent = `${i}`;
+        grid.addEventListener("mouseover", hover)
         container.appendChild(grid);
     }
 }
+/*
+let items = document.querySelector(".box7");
+items.
+console.log(items); 
 
+for (let i = 0; i < items.length, i++;){
+    let data = items[i];
+    data.addEventListener("mouseover", function(event){
+        console.log(items[i]);
+        console.log(event.target);
+        data.style.cssText = "background-color: blue";
+    });
+}
+*/
 window.onload = function(){
     grid();
 }
